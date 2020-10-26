3c4940de3bc964be90ac8cde0840a002351d8280
thang-tran <thang-tran@dimage.co.jp>
2020-10-26 13:54:38 +0700

    first pro

---
+++ src/DemoJava.java
@@ -0,0 +1,9 @@
+public class DemoJava {
+    public static void main(String[] args) {
+            String input = "Dimage Share";
+            char[] charArray = input.toCharArray();
+            for(int i =charArray.length-1 ;i>=0;i--){
+                System.out.print(charArray[i]);
+            }
+    }
+}
