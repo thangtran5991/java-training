1f26efa149f7ff632ff4980108807f8aad75e5bb
thang-tran <thang-tran@dimage.co.jp>
2020-10-30 14:38:48 +0700

    fix regex email

--- src/RegexEmail.java
+++ src/RegexEmail.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexEmail {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        Pattern pattern = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
-        Matcher matcher = pattern.matcher(text);
+        Pattern pattern = Pattern.compile("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b");
+        Matcher matcher = pattern.matcher("foobar@gmail.com");
         if (matcher.find()) {
             System.out.println("Correct!");
         } else {
