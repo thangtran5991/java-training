fa4854e0333612ea933ac5e1e8a5e28052f3e83b
thang-tran <thang-tran@dimage.co.jp>
2020-11-03 15:11:27 +0700

    group string

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        StringBuffer stringBuffer = new StringBuffer("Java");
-        for (int i = 0; i < 100000; i++) {
-            stringBuffer.append("Hello");
+        Test t1 = new Test();
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("abc");
+        arrayList.add("bcd");
+        arrayList.add("bbb");
+        arrayList.add("ace");
+        arrayList.add("snb");
+        arrayList.add("aaaa");
+        arrayList.add("bbbbb");
+        arrayList.add("eeee");
+        t1.groupString(arrayList);
+    }
+
+    public static List<String> groupString(List<String> input) {
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        ArrayList<String> arrayList3 = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            switch (input.get(i).charAt(0)) {
+                case 'a':
+                    arrayList1.add(input.get(i));
+                    break;
+                case 'b':
+                    arrayList2.add(input.get(i));
+                    break;
+                default:
+                    arrayList3.add(input.get(i));
+                    break;
+            }
         }
-        System.out.println("Time to concat string of StringBuffer: "
-                + (System.currentTimeMillis() - startTime) + "ms");
-        startTime = System.currentTimeMillis();
-        StringBuilder stringBuilder = new StringBuilder("Java");
-        for (int i = 0; i < 100000; i++) {
-            stringBuilder.append("Hello");
-        }
-        System.out.println("Time to concat string of StringBuilder: "
-                + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.print(arrayList1);
+        System.out.print(arrayList2);
+        System.out.print(arrayList3);
+
+        return input;
     }
 }
