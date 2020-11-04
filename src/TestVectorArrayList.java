395871220d016129189192899fdb8c9911f3272d
thang-tran <thang-tran@dimage.co.jp>
2020-11-04 14:43:56 +0700

    vector and arraylist

--- src/TestVectorArrayList.java
+++ src/TestVectorArrayList.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
 import java.util.Vector;
 
-public class TestVectorArrayList {
+public class TestVectorArrayList extends Thread{
+    public void run() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        Vector<String> vector = new Vector<>();
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+        arrayList.add("d");
+        System.out.println(arrayList);
+        vector.add("a");
+        vector.add("b");
+        vector.add("c");
+        vector.add("d");
+        System.out.println(vector);
+    }
+
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < 1000000; i++) {
-            arrayList.add(i);
-        }
-        System.out.println("Time to add value to ArrayList: " + (System.currentTimeMillis() - startTime));
-        startTime = System.currentTimeMillis();
-        Vector<Integer> vector = new Vector<>();
-        for (int i = 0; i< 1000000; i++) {
-            vector.add(i);
-        }
-        System.out.println("Time to add value to Vector: " + (System.currentTimeMillis() - startTime));
+        TestVectorArrayList t1 = new TestVectorArrayList();
+        TestVectorArrayList t2 = new TestVectorArrayList();
+
+        t1.start();
+        t2.start();
     }
 }
