a431fc70d34c4729a7fd5622d07b2605bdefe94f
thang-tran <thang-tran@dimage.co.jp>
2020-11-13 17:09:50 +0700

    demo

---
+++ src/GenericDemo.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericDemo {
+    public static void main(String[] args) {
+        KeyValuePair<String, Integer> keyValuePair = new KeyValuePair<>("Tran Hue Thang", 1995);
+        String name = keyValuePair.getKey();
+        Integer id = keyValuePair.getValue();
+        System.out.println("Name: " + name + ", Id = " + id);
+    }
+}
---
+++ src/ThreadDemo.java
@@ -0,0 +1,10 @@
+public class ThreadDemo extends Thread{
+    public void run() {
+        System.out.println("Thread is running...");
+    }
+
+    public static void main(String[] args) {
+        ThreadDemo t1 = new ThreadDemo();
+        t1.start();
+    }
+}
