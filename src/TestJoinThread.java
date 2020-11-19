b86bb757c668d1b8a4ec90c474b447f6d44c7c56
thang-tran <thang-tran@dimage.co.jp>
2020-11-19 17:14:38 +0700

    thread

---
+++ src/TestJoinThread.java
@@ -0,0 +1,30 @@
+public class TestJoinThread {
+    public static void main(String[] args) {
+        JoinThread thread1 = new JoinThread();
+        thread1.setName("Thread 1");
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JoinThread thread2 = new JoinThread();
+        thread2.setName("Thread 2");
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JoinThread thread3 = new JoinThread();
+        thread3.setName("Thread 3");
+        thread3.start();
+        try {
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -1,10 +1,28 @@
 public class ThreadDemo extends Thread{
+    private Thread t;
+    private String threadName;
+    ThreadDemo(String name) {
+        threadName = name;
+        System.out.println("Creating " + threadName);
+    }
     public void run() {
-        System.out.println("Thread is running...");
+        System.out.println("Running " + threadName);
+        try {
+            for (int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + " interrupt");
+        }
+        System.out.println("Thread " + threadName + " exist");
     }
 
-    public static void main(String[] args) {
-        ThreadDemo t1 = new ThreadDemo();
-        t1.start();
+    public void start() {
+        System.out.println("Starting " + threadName);
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
     }
 }
