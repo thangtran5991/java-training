fa1cd214d776314f6bde692d71cdf36954394991
thang-tran <thang-tran@dimage.co.jp>
2020-11-20 17:14:27 +0700

    thread demo

---
+++ src/SynchronizedDemo.java
@@ -0,0 +1,7 @@
+public class SynchronizedDemo {
+    public static synchronized void printData(String threadName) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + ": " + i);
+        }
+    }
+}
---
+++ src/ThreadDemo1.java
@@ -0,0 +1,11 @@
+public class ThreadDemo1 extends Thread{
+    private SynchronizedDemo synchronizedDemo;
+    private String threadName;
+    public ThreadDemo1(SynchronizedDemo synchronizedDemo, String threadName) {
+        this.synchronizedDemo = synchronizedDemo;
+        this.threadName = threadName;
+    }
+    public void run() {
+        synchronizedDemo.printData(threadName);
+    }
+}
