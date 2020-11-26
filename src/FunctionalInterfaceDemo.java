4877bc4fc351951df9c75f19f13fb52d9605fe58
thang-tran <thang-tran@dimage.co.jp>
2020-11-26 17:03:33 +0700

    java8

---
+++ src/FunctionalInterfaceDemo.java
@@ -0,0 +1,16 @@
+public class FunctionalInterfaceDemo {
+    interface FunctionalInterface1 {
+
+    }
+    @FunctionalInterface
+    public interface Add extends FunctionalInterface1{
+        void doSomething();
+        default void defaultMethod() {
+
+        };
+        static void staticMethod() {
+
+        }
+
+    }
+}
---
+++ src/StreamException.java
@@ -0,0 +1,11 @@
+import java.util.stream.Stream;
+
+public class StreamException {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("Java", "C#", "PHP")
+                .filter(s -> s.startsWith("J"));
+        stream.anyMatch(s -> true);
+        stream.noneMatch(s -> true);
+
+    }
+}
